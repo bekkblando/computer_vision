5f57b254d8ce412a6148fea27999547945b4002e
bekkblando <bekkblando@gmail.com>
2020-03-05 19:11:12 -0500

    frame cohesion working

--- assignment5/find_faces.py
+++ assignment5/find_faces.py
@@ -5,12 +5,11 @@ import ffmpy
 import sys
 import os
 import math
+import time
 
+class Face():
     
-class Faces():
-    
-    def __init__(self):
-        self.face_cascade = cv2.CascadeClassifier('classifiers/frontalFace10/haarcascade_frontalface_alt2.xml')
+    def __init__(self, rect, gray, index):
         self.left_eye_cascade = cv2.CascadeClassifier(f'{cv2.data.haarcascades}/haarcascade_lefteye_2splits.xml')
         self.right_eye_cascade = cv2.CascadeClassifier(f'{cv2.data.haarcascades}/haarcascade_righteye_2splits.xml')
 
@@ -26,68 +25,107 @@ class Faces():
             
         self.nose_cascade = cv2.CascadeClassifier(self.noseCascadeName)
         self.mouth_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + self.mouthCascadeName)
+        self.rect = rect
+        (x,y,w,h) = rect
+        self.frame = frame
+        self.filtered_grey = gray[y: y + h, x: x + w]
+        self.color = frame[y: y + h, x: x + w]
+        self.updated = time.time()
+        self.index = index
         
-    def draw_cascades(self, items, frame, label):
-        for index, (x,y,w,h) in enumerate(items):
-            cv2.rectangle(frame,(x, y),(x + w, y + h),(0,255,0),2)
-            cv2.putText(frame,f'{label} {index}',(x + x, y + y), 1, 1, (0, 255, 0), 1)
-          
-    def in_face(self, obj):
-        for (x,y,w,h) in self.faces:
-            if(obj[0] >= x and obj[1] >= y):
-                return True
-        return False
+    def get_left_eyes(self):
+        return self.left_eye_cascade.detectMultiScale(self.filtered_grey, 1.1, minNeighbors=35)
     
-    def remove_not_in_face(self, objs):
-        return list(filter(self.in_face, objs))
+    def get_right_eyes(self):
+        return self.right_eye_cascade.detectMultiScale(self.filtered_grey, 1.1, minNeighbors=35)
+    
+    def get_mouths(self):
+        return self.mouth_cascade.detectMultiScale(self.filtered_grey, 1.1, minNeighbors=35, minSize=(25, 25))
+    
+    def get_noses(self):
+        return self.nose_cascade.detectMultiScale(self.filtered_grey, 1.1, minNeighbors=35)
+    
+    def update(self, rect, gray):
+        # Update Time
+        self.rect = rect
+        self.gray = gray
+        self.frame = frame
+        self.updated = time.time()
+
+    def draw_cascades(self, items, frame, label):
+        for (x,y,w,h) in items:
+            # Change the thickness of the line to give a fade effect
+            cv2.rectangle(frame,(x, y),(x + w, y + h),(0,255,0), math.floor(3 - self.decay()))
+            cv2.putText(frame,f'{label}',(x + x, y + y), 1, 1, (0, 255, 0), 1)
+
+    def decay(self):
+        return time.time() - self.updated
+    
+    def draw(self, frame):
+        self.frame = frame
+        (x,y,w,h) = self.rect
+        self.color = frame[y: y + h, x: x + w]
+        
+        # Draw Face
+        self.draw_cascades([self.rect], frame, f'Face {self.index}')
+        
+        # Draw a rectangle around the left eyes
+        self.draw_cascades(self.get_left_eyes(), self.color, "Left Eye")
+
+
+        # Draw a rectangle around the right eyes
+        self.draw_cascades(self.get_right_eyes(), self.color, "Right Eye")
+
+
+        # Draw a rectangle around the mouth
+        self.draw_cascades(self.get_mouths(), self.color, "Mouth")
+
+
+        # Draw a rectangle around the nose
+        self.draw_cascades(self.get_noses(), self.color, "Nose")
+
+
+class Faces():
+    
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier('classifiers/frontalFace10/haarcascade_frontalface_alt2.xml')
+        self.faces = []
+    
     
     def get_faces(self, frame):
         return self.face_cascade.detectMultiScale(frame, 1.1)
     
-    def get_left_eyes(self, frame):
-        return self.left_eye_cascade.detectMultiScale(frame, 1.1, minNeighbors=35)
     
-    def get_right_eyes(self, frame):
-        return self.right_eye_cascade.detectMultiScale(frame, 1.1, minNeighbors=35)
-    
-    def get_mouths(self, frame):
-        return self.mouth_cascade.detectMultiScale(frame, 1.1, minNeighbors=35, minSize=(25, 25))
-    
-    def get_noses(self, frame):
-        return self.nose_cascade.detectMultiScale(frame, 1.1, minNeighbors=35)
+    def get_distance(self, x1, y1, x2, y2):
+        return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
     
     
     def read_clean_view(self, gray, frame):
-        self.faces = []
-        self.left_eyes = []
-        self.right_eyes = []
-        self.mouths = []
-        self.noses = []
+        self.same_for = 300
+        self.decayed = 3
         
         # Get those rectangles around those faces
-        self.faces = self.get_faces(gray)
-        self.draw_cascades(self.faces, frame, "Face")
         
-        for (x, y, w, h) in self.faces:
-            filtered_grey = gray[y: y + h, x: x + w]
-            color = frame[y: y + h, x: x + w]
-
-            self.draw_cascades(self.get_left_eyes(filtered_grey), color, "Left Eye")
-            # Draw a rectangle around the left eyes
-           
-
-            # Draw a rectangle around the right eyes
-            self.draw_cascades(self.get_right_eyes(filtered_grey), color, "Right Eye")
-            
-
-            # Draw a rectangle around the mouth
-            self.draw_cascades(self.get_mouths(filtered_grey), color, "Mouth")
-
-
-            # Draw a rectangle around the nose
-            self.draw_cascades(self.get_noses(filtered_grey), color, "Nose")
-    
-     
+        faces = self.get_faces(gray)
+        self.faces = list(filter(lambda face: face.decay() < self.decayed, self.faces))
+                
+        # Find the same faces
+        for new_face in faces:
+            new = True
+            for face in self.faces:
+                # If the same face update
+                if(self.get_distance(new_face[0], new_face[1], face.rect[0], face.rect[1]) < self.same_for):
+                    face.update(new_face, gray)
+                    new = False
+                    
+            if(new):
+                face = Face(new_face, gray, len(self.faces))
+                self.faces.append(face)
+                face.draw(frame)
+        
+        for face in self.faces:
+            face.draw(frame)
+                
 
 def get_frames(vid):
     frames = []
